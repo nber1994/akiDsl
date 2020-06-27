e0f8dc609788fb5482fc8c4ce0c83f7f3b51ae44
jingtianyou <jingtianyou@didiglobal.com>
2020-06-28 01:24:48 +0800

    fix bug

--- runCxt/runCxt.go
+++ runCxt/runCxt.go
@@ -2,6 +2,7 @@ package runCxt
 
 import (
     "fmt"
+    "encoding/json"
 )
 
 
@@ -16,7 +17,8 @@ func NewRunCxt() *RunCxt{
 
 //获取值
 func (this *RunCxt) GetValue(varName string) interface{}{
-    fmt.Println("get var ", varName)
+    fmt.Println("+++ get var ", varName)
+    fmt.Println("+++ now var ", this.ToString())
     if _, exist := this.Vars[varName]; !exist {
         panic("syntax error: not exist var")
     }
@@ -25,7 +27,16 @@ func (this *RunCxt) GetValue(varName string) interface{}{
 
 //设置值
 func (this *RunCxt) SetValue(varName string, value interface{}) bool {
-    fmt.Println("set var ", varName, value)
+    fmt.Println("+++ set var ", varName, value)
+    fmt.Println("+++ now var ", this.ToString())
     this.Vars[varName] = value
     return true
+}
+
+func (this *RunCxt) ToString() string {
+    jsonStr, err := json.Marshal(this.Vars)
+    if err != nil {
+        panic("run time cxt string error")
+    }
+    return string(jsonStr)
 }
