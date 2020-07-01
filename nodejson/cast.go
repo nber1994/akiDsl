8ec9f1f269c3c6e31a01ebfe49fb789653e87c9f
jingtianyou <jingtianyou@didiglobal.com>
2020-07-01 17:10:04 +0800

    增加nil

--- nodejson/cast.go
+++ nodejson/cast.go
@@ -36,6 +36,7 @@ func (n Node) Interface() interface{} {
 }
 
 func (n *Node) IntE() (int, error) {
+    var nilRet int
 	switch n.data.(type) {
 	case json.Number:
 		i, err := n.data.(json.Number).Int64()
@@ -47,10 +48,11 @@ func (n *Node) IntE() (int, error) {
 	case uint, uint8, uint16, uint32, uint64:
 		return int(reflect.ValueOf(n.data).Uint()), nil
 	}
-	return 0, errors.New("invalid value type")
+	return nilRet, errors.New("invalid value type")
 }
 
 func (n *Node) Float64E() (float64, error) {
+    var nilRet float64
 	switch n.data.(type) {
 	case json.Number:
 		return n.data.(json.Number).Float64()
@@ -61,10 +63,11 @@ func (n *Node) Float64E() (float64, error) {
 	case uint, uint8, uint16, uint32, uint64:
 		return float64(reflect.ValueOf(n.data).Uint()), nil
 	}
-	return 0, errors.New("invalid value type")
+	return nilRet, errors.New("invalid value type")
 }
 
 func (n *Node) Int64E() (int64, error) {
+    var nilRet int64
 	switch n.data.(type) {
 	case json.Number:
 		return n.data.(json.Number).Int64()
@@ -75,21 +78,23 @@ func (n *Node) Int64E() (int64, error) {
 	case uint, uint8, uint16, uint32, uint64:
 		return int64(reflect.ValueOf(n.data).Uint()), nil
 	}
-	return 0, errors.New("invalid value type")
+	return nilRet, errors.New("invalid value type")
 }
 
 func (n *Node) StringE() (string, error) {
+    var nilRet string
 	if s, ok := (n.data).(string); ok {
 		return s, nil
 	}
-	return "", errors.New("type assertion to string failed")
+	return nilRet, errors.New("type assertion to string failed")
 
 }
 
 func (n *Node) BoolE() (bool, error) {
+    var nilRet bool
 	if s, ok := (n.data).(bool); ok {
 		return s, nil
 	}
-	return false, errors.New("type assertion to bool failed")
+	return nilRet, errors.New("type assertion to bool failed")
 }
 
